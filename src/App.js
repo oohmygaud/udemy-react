cbc15fa8e538278bdea2e22b0c9b255265dc8dd3
Audrey <a@lwb.co>
2019-03-18 11:37:22 -0400

    Initialize repository and work on App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Audrey', age: 28 },
+      { name: 'Lee', age: 31 },
+      { name: 'Susan', age: 66 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi. I'm a React App</h1>
+        <p>This is working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name}  age={this.state.persons[1].age}>My Hobbies: Pooping</Person>
+        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age} />
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
